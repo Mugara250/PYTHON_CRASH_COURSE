1b498be989b72273d8a47deaecf0c599b54bcff7
Mugara374 <m.mushi@alustudent.com>
2024-02-02 18:33:59 +0200

    strings

---
+++ strings.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# first_name = "mushi"
+# last_name = "mbonyumugara"
+# full_name = f"{first_name} {last_name}"
+
+# greetings = f"Hello, {full_name.title()}"
+# print(greetings)
+
+# first_name = " boris "
+# last_name = " nyilindekwe "
+# full_name = f"{first_name.strip()} {last_name.strip()}"
+
+# greetings = f"Hello,\n\t{full_name.title()}"
+# print(greetings)
+
+# youtube_url = "https://youtube.com"
+# youtube_url = youtube_url.removeprefix('https://')
+# print(youtube_url)
+
+apostrophe = "This is isn't about how intelligent or smart you are."
+print(apostrophe)
